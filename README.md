666329b7537e499dc54f0c1a02f3fccfb0c843fc
Brian Rhea <brianrhea@gmail.com>
2014-01-23 17:14:21 -0700

    add files

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-droplist
-========
+# Droplist Plugin for jQuery
 
-jQuery plugin that converts a breadcrumb trail or folder tree (or any &lt;ul>) in to a dynamic dropdown menu, but only when you've run out of horizontal space.
+Droplist is a plugin that converts a breadcrumb trail or folder tree (or any `<ul>`) in to a dynamic dropdown menu, but only when you've run out of horizontal space.
+
+## Demo
+
+Examples available at [brianrhea.github.io/droplist](http://brianrhea.github.io/droplist)
+
+## How to Use
+
+ - Download and include the javascript source in your document: `/path/to/jquery.droplist.min.js`
+ - As well as the css: `/path/to/jquery.droplist.min.css`
+ - Add `droplist` as a class to any `<ul>` in the document
+ - Specify a max-width for the `<ul class="droplist">` in your own css source, i.e.:
+
+```css
+.droplist {
+  width: 200px;
+}
+```
+ - Call .droplist()
+
+```javascript
+$(document).ready(function(){
+  $('.droplist').droplist();
+});
+```
+
+## Options
+
+**overflow**
+Where should Droplist enforce the defined width and create the dropdown?
+```
+default: 'left'
+options: 'left', 'right'
+```
+
+**ellipsis**
+What should Droplist show after the last visible `<li>`? Set to `false` to hide the ellipsis.
+```
+default: '&#8230;'
+options: 'string', false
+```
+
+**dropicon**
+If you're using the [Font Awesome](http://fontawesome.io) icon library, Droplist allows you to pass in a font-awesome class that will be used for the dropdown icon. If you aren't using FA, don't worry, Droplist defaults to false. If you are, and want to use a particular icon, just pass the [icon class](http://fontawesome.io/icons/) and Droplist will take care of the rest.
+
+```
+default: false
+options: 'fa-toggle-down', 'fa-toggle-right', 'fa-plus-square', etc
+```
+
+#### Example
+
+The following code tells Droplist to overflow on the right, hide the ellipsis, and use Font Awesome's fa-toggle-right icon as the dropdown.
+
+```javascript
+$(document).ready(function(){
+  $('.droplist').droplist({
+    overflow: 'right',
+    ellipsis: false,
+    dropicon: 'fa-toggle-right'
+  });
+});
+```
+
+## License
+
+All code licensed under the [MIT License](http://www.opensource.org/licenses/mit-license.php). In other words you are basically free to do whatever you want. Please don't remove my name from the source code, but who does that anyway?
---
+++ js/jquery.droplist.js
@@ -0,0 +1,95 @@
+(function ( $ ) {
+
+	$.fn.droplist = function(options) {
+
+		var settings = $.extend({
+			overflow: "left",
+            ellipsis: "&#8230;",
+            dropicon: "fa-toggle-down"
+        }, options );
+
+		this.each(function(){
+			var createDropList			= false;
+			var width					= $(this).width() - 26;
+			var children				= $.makeArray($(this).find('li'));
+			var childrenWithWidths		= [];
+			var childrenWidthTotal		= 0;
+			var hiddenChildren			= [];
+
+			_.each(children, function(child){
+				childrenWidthTotal += $(child).outerWidth();
+				childrenWithWidths.push([child, $(child).outerWidth()]);
+			});
+
+			if ( settings.overflow === "left" ) {
+
+				$('.droplist').addClass('droplist-left');
+
+				if ( childrenWidthTotal > width ) {
+					childrenWidthTotal = 0;
+					for ( var i = childrenWithWidths.length - 1; i >= 0; i-- ) {
+						childrenWidthTotal += childrenWithWidths[i][1];
+						if ( childrenWidthTotal > width ) {
+							hiddenChildren.push(childrenWithWidths[i][0]);
+							$(childrenWithWidths[i][0]).remove();
+							createDropList = true;
+						}
+					}
+				}
+
+				if ( createDropList === true ) {
+
+					var html = [];
+
+					if ( settings.ellipsis ) {
+						$(this).prepend('<li class=\"droplist-ellipsis\">' + settings.ellipsis + '</li>');
+					}
+
+					$(this).prepend('<li class=\"droplist-dropdown dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"fa ' + settings.dropicon + '\"></i></a><ul class=\"dropdown-menu\"></ul></li>');
+
+					_.each(hiddenChildren, function(hiddenChild){
+						html.push(hiddenChild.outerHTML);
+					})
+
+					$(this).find('.dropdown-menu').append( html.join('') );
+				}
+
+			} else {
+
+				$('.droplist').addClass('droplist-right');
+
+				if ( childrenWidthTotal > width ) {
+					childrenWidthTotal = 0;
+					for ( var i = 0; i < childrenWithWidths.length; i++ ) {
+						childrenWidthTotal += childrenWithWidths[i][1];
+						if ( childrenWidthTotal > width ) {
+							hiddenChildren.push(childrenWithWidths[i][0]);
+							$(childrenWithWidths[i][0]).remove();
+							createDropList = true;
+						}
+					}
+				}
+
+				if ( createDropList === true ) {
+
+					var html = [];
+
+					if ( settings.ellipsis ) {
+						$(this).append('<li class=\"droplist-ellipsis\">' + settings.ellipsis + '</li>');
+					}
+
+					$(this).append('<li class=\"droplist-dropdown dropdown\"><a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"fa ' + settings.dropicon + '\"></i></a><ul class=\"dropdown-menu\"></ul></li>');
+
+					_.each(hiddenChildren, function(hiddenChild){
+						html.push(hiddenChild.outerHTML);
+					})
+
+					$(this).find('.dropdown-menu').append( html.join('') );
+				}
+
+			}
+
+		});
+	};
+
+}( jQuery ));
